c74135269a65ae96eb7aa43aeaec2cebbb98ecab
Esteve <esteve.17mas@gmail.com>
2024-08-17 10:20:39 +0200

    feat: problem solved with O(n) complexity

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
-//-10^9 <= target <= 10^9
-function isValidNum(num: number) {
-  const maxValue = Math.pow(10, 9);
-  const minValue = maxValue * -1;
-  return minValue <= num && num <= maxValue;
-}
-
 function twoSum(nums: number[], target: number) {
   const totalLength = nums.length;
+  const maxValue = Math.pow(10, 9);
+  const minValue = maxValue * -1;
 
   //2 <= nums.length <= 10^4
   if (totalLength < 2 || totalLength > Math.pow(10, 4)) return [];
 
-  for (let i = 0; i < nums.length - 1; i++) {
-    const op1 = nums[i];
-    if (!isValidNum(op1)) return [];
-    for (let j = i + 1; j < nums.length; j++) {
-      const op2 = nums[j];
-      if (!isValidNum(op2)) return [];
-      if (op1 + op2 === target) return [i, j];
-    }
+  const numIdx: { [key: number]: number } = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentValue = nums[i];
+
+    //-10^9 <= target <= 10^9
+    if (currentValue < minValue || currentValue > maxValue) return [];
+
+    const complement = target - currentValue;
+    if (numIdx.hasOwnProperty(complement)) return [i, numIdx[complement]];
+
+    numIdx[currentValue] = i;
   }
 }
 
